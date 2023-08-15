aaa96f9c2c33277f07fff58b7ba3be5dd5175a0f
tarabaria <128685314+tarabaria@users.noreply.github.com>
2023-08-15 22:47:59 +0300

    +

--- js/task-02.js
+++ js/task-02.js
@@ -2,15 +2,7 @@ const ingredients = [
   'Potatoes',
   'Mushrooms',
   'Garlic',
-  'Tomatoes',
+  'Tomatos',
   'Herbs',
   'Condiments',
 ];
-
- const ingredientsList = document.querySelector("#ingredients");
-    ingredients.forEach(ingredient => {
-      const li = document.createElement("li");
-      li.textContent = ingredient;
-      li.classList.add("item");
-      ingredientsList.appendChild(li); 
-    });
